cad04b0e0b86d2b5069614f4318c452a8f2f22a6
DarkGeekMS <mohamedshawky911@gmail.com>
2021-01-10 09:03:28 +0200

    message generation through fixed set of messages

--- gen_messages.py
+++ gen_messages.py
@@ -1,14 +1,31 @@
-from essential_generators import DocumentGenerator
 import sys
 import os
 import random
 
 node_count = sys.argv[1]
 root_dir = "Network_project/simulations/msg_files"
-gen = DocumentGenerator()
+messages_corpus = [
+    "Hello sweet",
+    "how are you?",
+    "are you fine?!",
+    "great nice seeing ya",
+    "wanna coffee?",
+    "I hate you",
+    "I love you",
+    "just kidding",
+    "good bye",
+    "good for ya",
+    "fine!",
+    "Hello friend",
+    "thanks a lot for your help",
+    "thanks in advance!"
+]
 
 for idx in range(int(node_count)):
     with open(os.path.join(root_dir, f"{idx}.txt"), "w+") as f:
-        message_count = random.randint(10, 20)
+        message_count = random.randint(20, 50)
         for message in range(message_count):
-            f.write(gen.sentence()+"\n")
+            if message != message_count - 1:
+                f.write(random.choice(messages_corpus)+"\n")
+            else:
+                f.write(random.choice(messages_corpus))
